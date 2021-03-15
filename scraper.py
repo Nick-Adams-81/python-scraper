2e741db1c35853c32f188b234074c124347af9e7
nick adams <thebigdeal916@icloud.com>
2021-03-15 15:54:44 -0500

    variable re name

--- scraper.py
+++ scraper.py
@@ -16,13 +16,13 @@ with open('Texas high school recruits(class of 2022).csv', 'w') as csv_file:
     csv_writer.writerow(headers)
 
     for item in posts:
-        names = list(item.stripped_strings)[1]
-        ranks = list(item.stripped_strings)[0]
+        name = list(item.stripped_strings)[1]
+        rank = list(item.stripped_strings)[0]
         position = list(item.stripped_strings)[4]
         position_rank = list(item.stripped_strings)[5]
         school = list(item.stripped_strings)[2]
         commited = list(item.stripped_strings)[3]
-        csv_writer.writerow([names, ranks, school, position, position_rank, commited])
+        csv_writer.writerow([name, rank, school, position, position_rank, commited])
         
    
         
