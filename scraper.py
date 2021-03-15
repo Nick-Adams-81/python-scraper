b14cf08469c2be0e1d393702ea964eb846509dc0
nick adams <thebigdeal916@icloud.com>
2021-03-15 15:42:58 -0500

    update

--- scraper.py
+++ scraper.py
@@ -9,21 +9,20 @@ response = requests.get('https://www.texasfootball.com/recruiting/rankings/?ref=
 soup = BeautifulSoup(response.text, 'html.parser')
 
 posts = soup.find(class_='c-rcrt-rankings__list')
-print(posts)
-with open('recruits(class of 2021).csv', 'w') as csv_file:
+
+with open('Texas high school recruits(class of 2022).csv', 'w') as csv_file:
     csv_writer = writer(csv_file)
-    headers = ['Name', 'Rank', 'Commited', 'Position', 'Position rank', 'School']
+    headers = ['Name', 'Rank', 'School', 'Position', 'Position rank', 'Commited']
     csv_writer.writerow(headers)
 
     for item in posts:
-        names = item.find('span').get_text()
-        ranks = item.find(class_='c-rcrt-rankings__list-player-col c-rcrt-rankings__list-player-col-rank').get_text()
+        names = list(item.stripped_strings)[1]
+        ranks = list(item.stripped_strings)[0]
         position = list(item.stripped_strings)[4]
         position_rank = list(item.stripped_strings)[5]
         school = list(item.stripped_strings)[2]
-        commited = item.find(class_='c-rcrt-rankings__list-player-commit-mbl').get_text()
-        #print(list(item.stripped_strings))
-        csv_writer.writerow([names, ranks, commited, position, position_rank, school])
+        commited = list(item.stripped_strings)[3]
+        csv_writer.writerow([names, ranks, school, position, position_rank, commited])
         
    
         
