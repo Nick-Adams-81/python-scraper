2a4cfb0ecf3bcecb1ab8ace98fd83b6848dca36e
nick adams <thebigdeal916@icloud.com>
2021-03-24 21:46:57 -0500

    all state init commit

--- all_state.py
+++ all_state.py
@@ -2,3 +2,7 @@ from selenium import webdriver
 from csv import writer
 import pandas as pd
 import time
+
+driver = webdriver.Chrome('/chromedriver')
+url = 'https://www.dallasnews.com/high-school-sports/football/2021/02/26/the-2020-21-texas-sports-writers-association-class-6a-all-state-high-school-football-team/'
+driver.get(url)
